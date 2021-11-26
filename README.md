43907acae994e1c1adb3ce853b459501cb4f6c92
Fabian Flores <spanishbomb8@gmail.com>
2021-11-26 16:48:38 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# JSConf Challenge
+
+##
+
+- Create a branch with the following structure for the name `feature/yourname`.
+- Inside the `index.ts` file, you will find the challenge's instructions.
+- it would be awesome if you add some test cases for your algorithm.
+- Once you finish the challenge, create a pull request and wait for the results.
+
+## Requirements
+
+You need to have the following services installed in your computer:
+
+- node > 14
+
+### Mac OS
+
+```
+brew install node
+```
+
+### Clone the repo
+
+```
+git clone https://github.com/jitt3/jsconf-challenge.git
+```
+
+### Install node dependencies
+
+```
+yarn
+```
+
+or
+
+```
+npm install
+```
+
+## Tech Stack
+
+- Typescript
---
+++ src/index.ts
@@ -0,0 +1,26 @@
+/*
+Te hemos proporcionado un string que contiene una lista de enteros  positivos
+separados por un espacio cada uno(" ")
+Toma cada valor y calcula la soma de sus digitos, y lo llamaremos "weight".
+Debes de regresar como resultado la lista en orden ascendente por "weight",
+como un string y separados por un espacio
+
+Tomemos el siguiente ejemplo:
+el número 99 tendra un "weight" de 18, mientras que 100 tendra un "weight" de 1
+de esta manera el numero 100 debera ir antes de 99
+
+Input 
+"56 65 74 100 99 68 86 180 90" 
+
+Output
+"100 180 90 56 65 74 68 86 99"
+
+Cuando dos numeros tengan el mismo "weight", consideralos como string en lugar de números:
+Por ejemplo
+El número 100 deber ir antes de 180 porque su "weight" (1) es menor que el de 180 (9)
+y 180 va antes de 90 pues como tienen el mismo "weight" que es 9, si es  tomado como string
+entonces debe ir antes.
+
+Nota: todos los números de la lista son positivos y la lista puede estar vacia.
+
+*/
