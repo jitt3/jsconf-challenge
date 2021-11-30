495ebcfb158bf4d4b24f410028341cedc4007b5a
Luis <leip1493@gmail.com>
2021-11-30 14:52:16 -0400

    Result

--- src/index.ts
+++ src/index.ts
@@ -24,3 +24,47 @@ entonces debe ir antes.
 Nota: todos los números de la lista son positivos y la lista puede estar vacia.
 
 */
+
+interface NumberWeight {
+  number: string;
+  weight: number;
+}
+
+export function getOrderedNumbersByWeight(input: string): string {
+  const strNumbers: string[] = parseStringToArray(input);
+
+  const numbersWeight: NumberWeight[] = mapToNumberWeight(strNumbers);
+
+  const orderedByWeightAsc: NumberWeight[] =
+    orderByWeightAscendant(numbersWeight);
+
+  return orderedByWeightAsc.map((elem) => elem.number).join(" ");
+}
+
+function parseStringToArray(input: string): string[] {
+  return input.split(" ");
+}
+
+function orderByWeightAscendant(numbersWeight: NumberWeight[]): NumberWeight[] {
+  return numbersWeight.sort((a, b) => {
+    if (a.weight !== b.weight) {
+      return a.weight - b.weight;
+    }
+    if (a.number > b.number) {
+      return 1;
+    }
+    if (a.number < b.number) {
+      return -1;
+    }
+    return 0;
+  });
+}
+function mapToNumberWeight(strNumbers: string[]): NumberWeight[] {
+  return strNumbers.map((strNumber: string) => ({
+    number: strNumber,
+    weight: calculateWeigth(strNumber),
+  }));
+}
+function calculateWeigth(strNumber: string): number {
+  return strNumber.split("").reduce((total, number) => total + +number, 0);
+}
---
+++ test/index.test.ts
@@ -0,0 +1,28 @@
+import { getOrderedNumbersByWeight } from "../src/index";
+
+describe("getOrderedNumbersByWeight", () => {
+  test("Example provided", () => {
+    const input: string = "56 65 74 100 99 68 86 180 90";
+    const output: string = "100 180 90 56 65 74 68 86 99";
+    expect(getOrderedNumbersByWeight(input)).toBe(output);
+  });
+
+  test("Natural numbers", () => {
+    const input: string = "1 2 3 4 5";
+    const output: string = "1 2 3 4 5";
+    expect(getOrderedNumbersByWeight(input)).toBe(output);
+  });
+
+  test("No input", () => {
+    const input: string = "";
+    const output: string = "";
+    expect(getOrderedNumbersByWeight(input)).toBe(output);
+  });
+
+  test("Same weight", () => {
+    const input: string = "180 9 180 72 72";
+    const output: string = "180 180 72 72 9";
+    const resp = getOrderedNumbersByWeight(input);
+    expect(resp).toBe(output);
+  });
+});
